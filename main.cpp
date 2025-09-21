1605868a030dddb456de5f5e9f46fd5386ceb5ab
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-21 03:01:53 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <array>
+#include <algorithm>
+#include <numeric> 
 #include <fstream>
 #include <iostream>
 
@@ -6,6 +8,14 @@ using namespace std;
 
 constexpr size_t DAYS = 30;
 using Arr = array<int, DAYS>;
+
+
+int sum_c(int* a, size_t n) {
+    int s = 0;
+    for (size_t i = 0; i < n; ++i) s += a[i];
+    return s;
+}
+
 
 int main(){
     Arr steps{};
@@ -19,7 +29,22 @@ int main(){
         if (!(fin >> steps[i])) { cerr << "Not enough numbers\n"; return 1; }
     }
 
-    cout << "Loaded " << steps.size() << "days. First=" << steps.front() << " Last=" << steps.back() << "\n";
+    cout << "Size = " << steps.size() << " days. First=" << steps.front() << " Last=" << steps.back() << "\n";
+
+    try {
+        cout << "Day 10 (at) = " << steps.at(9) << "\n";
+    } catch(out_of_range& e){
+        cout << "Out of range: " << e.what() << "\n";
+    }
+
+    cout << "Empty? " << (steps.empty() ? "true" : "false") << "\n";
+
+    int *raw = steps.data();
+    cout << "data() pointer: " << raw << "\n"; //for the location
+
+    cout << "Max: " << *max_element(steps.begin(), steps.end()) << endl;
+    cout << "Min: " << *min_element(steps.begin(), steps.end()) << endl;
+    cout << "Sum: " << accumulate(steps.begin(), steps.end(), 0) << endl;
 
 }
 
