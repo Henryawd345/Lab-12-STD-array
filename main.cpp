1d72e8b207b1779f38df7454ee4f80d5a8bf6cc2
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-21 03:24:51 -0700

    update (finished)

--- main.cpp
+++ main.cpp
@@ -9,14 +9,6 @@ using namespace std;
 constexpr size_t DAYS = 30;
 using Arr = array<int, DAYS>;
 
-
-int sum_c(int* a, size_t n) {
-    int s = 0;
-    for (size_t i = 0; i < n; ++i) s += a[i];
-    return s;
-}
-
-
 int main(){
     Arr steps{};
     ifstream fin("steps.txt");
@@ -37,6 +29,17 @@ int main(){
         cout << "Out of range: " << e.what() << "\n";
     }
 
+    int looking = 6899;
+    array<int, DAYS>::iterator it;
+    it = find(steps.begin(), steps.end(), looking);
+    cout << "Find: " << looking;
+
+    if (it != steps.end())
+        cout << " found in position " << it - steps.begin() << endl;
+    else
+        cout << " was not found.\n";
+    cout << "    Value: " << *it << endl;
+
     cout << "Empty? " << (steps.empty() ? "true" : "false") << "\n";
 
     int *raw = steps.data();
@@ -46,5 +49,19 @@ int main(){
     cout << "Min: " << *min_element(steps.begin(), steps.end()) << endl;
     cout << "Sum: " << accumulate(steps.begin(), steps.end(), 0) << endl;
 
+    sort(steps.begin(), steps.end());
+    cout << "Sorted: ";
+    for (double val : steps) cout << val << " "; cout << endl;
+    sort(steps.rbegin(), steps.rend());
+    cout << "Reverse Sorted: ";
+    for (double val : steps) cout << val << " "; cout << endl;
+
+    cout << "Days 15-17 BEFORE FILL: " << steps[14] << " " << steps[15] << " " << steps[16] << "\n";
+    fill(steps.begin() + 14, steps.begin() + 17, 0);
+    cout << "Days 15-17 AFTER FILL:  "<< steps[14] << " " << steps[15] << " " << steps[16] << "\n";
+
+    cout << "Days 12 and 13 BEFORE SWAP: " << steps[11] << " " << steps[12] << "\n";
+    iter_swap(steps.begin() + 11, steps.begin() + 12);
+    cout << "Days 12 and 13 AFTER SWAP: " << steps[11] << " " << steps[12] << "\n";
 }
 
